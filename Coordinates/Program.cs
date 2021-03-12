b38ceed1809585bd76a941b46ec270e940d9bc8f
CH-173 <tcherk.33@yandex.ru>
2021-03-12 22:45:31 +0700

    Tasks

---
+++ Coordinates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0, step;
+            double distance;
+            for (step = 0; step < 10; step++)
+            {
+                Console.WriteLine("Введите координату (С / Ю / З / В): ");
+                char way = char.Parse(Console.ReadLine());
+                if (way == 'С' || way == 'с')
+                {
+                    y = y + 1;
+                }
+                else if (way == 'Ю' || way == 'ю')
+                {
+                    y = y - 1;
+                }
+                else if (way == 'З' || way == 'з')
+                {
+                    x = x - 1;
+                }
+                else if (way == 'В' || way == 'в')
+                {
+                    x = x + 1;
+                }
+            }
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+            distance = Math.Sqrt(x * x + y * y);
+            Console.WriteLine("Расстояние от начала координат до точки составляет " + distance + " метров.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Months/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+                int monthNum = 0;
+                Console.WriteLine("Введите порядковый номер месяца: ");
+                monthNum = int.Parse(Console.ReadLine());
+                monthNum = monthNum - 1;
+                Console.WriteLine(months[monthNum]);
+            } while (true);
+        }
+    }
+}
